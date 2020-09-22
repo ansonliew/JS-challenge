a6b46305605e133a04e4050c06f8194aa62e83b4
ansonliew <57343638+ansonliew@users.noreply.github.com>
2020-09-22 20:06:51 +0800

    Create challenge-1.js

---
+++ challenge-1.js
@@ -0,0 +1,13 @@
+var lines = 4;
+var hashtag = "#";
+for (var i = 0; i < lines+1; i++){
+  document.write(hashtag);
+  hashtag += "#";
+  document.write("<br>");
+}
+for (var i = 0; i < lines; i++){
+  for(var j = 0; j < lines-i; j++){
+    document.write("#");
+  }
+  document.write("<br>");
+}
