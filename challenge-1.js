5423d18a42f36afb312b47286b7ca0c1d8abce88
ansonliew <57343638+ansonliew@users.noreply.github.com>
2020-09-22 21:13:15 +0800

    Update challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -1,13 +1,45 @@
-var lines = 4;
-var hashtag = "#";
-for (var i = 0; i < lines+1; i++){
-  document.write(hashtag);
-  hashtag += "#";
-  document.write("<br>");
-}
-for (var i = 0; i < lines; i++){
-  for(var j = 0; j < lines-i; j++){
-    document.write("#");
-  }
-  document.write("<br>");
-}
+//js challenge no.1
+// var lines = 4;
+// var hashtag = "#";
+// for (var i = 0; i < lines+1; i++){
+//   document.write(hashtag);
+//   hashtag += "#";
+//   document.write("<br>");
+// }
+// for (var i = 0; i < lines; i++){
+//   for(var j = 0; j < lines-i; j++){
+//     document.write("#");
+//   }
+//   document.write("<br>");
+// }
+
+//js challenge no.2
+// for (var i = 1 ; i < 101; i ++){
+//    if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("FizzBuzz")
+//   }
+//   else if ( i % 3 == 0){
+//     console.log("Fizz");
+//   }
+//   else if ( i % 5 == 0){
+//     console.log("Buzz");
+//   }
+//   else {
+//      console.log(i);
+//   }
+// }
+
+//js challenge no.3
+// var lines = 11;
+
+// for (var j=0; j<lines; j++){
+//   for (var i=0; i<lines; i++){
+//      if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 ==1)){
+//         document.write("#");
+//        }
+//   else {
+//     document.write("&nbsp;");
+//   }
+//   }
+//   document.write("<br>");
+// }
